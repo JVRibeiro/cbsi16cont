02e61bfe20d141ea9abee357d2487095ed110771
Victor Ribeiro <victor_ribeiro135@hotmail.com>
2017-01-28 16:52:42 -0300

    *

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 var nome_aluno = document.querySelector( '#nome_aluno' ),
     serie_aluno = document.querySelector( '#serie_aluno' ),
     valor_aluno = document.querySelector( '#valor_aluno' ),
+    alunos_cadastrados = document.querySelector( '#alunos_cadastrados' ),
 
-    cadastro_aluno = {};
+    nome_bens = document.querySelector( '#nome_bens' ),
+    tipo_bens = document.querySelector( '#tipo_bens' ),
+    valor_bens = document.querySelector( '#valor_bens' ),
+    bens_cadastrados = document.querySelector( '#bens_cadastrados' );
+
+    if ( window.localStorage.getItem('cadastro_aluno') ) alunos_cadastrados.innerHTML = localStorage.getItem('cadastro_aluno');
+
+    if ( window.localStorage.getItem('cadastro_bens') ) bens_cadastrados.innerHTML = localStorage.getItem('cadastro_bens');
+
 
 
     $( '#serie_aluno' ).on( 'click', function ()
@@ -22,14 +31,55 @@ var nome_aluno = document.querySelector( '#nome_aluno' ),
         && serie_aluno.value !== ''
         && valor_aluno.value !== '' )
       {
-        cadastro_aluno.nome = nome_aluno.value;
-        cadastro_aluno.serie = serie_aluno.value;
-        cadastro_aluno.valor = valor_aluno.value;
+        alunos_cadastrados.innerHTML +=
+           '<li>'
+           + '<div class="saved_nome">' + nome_aluno.value + '</div>'
+           + '<div class="saved_serie">' + serie_aluno.value + '</div>'
+           + '<div class="saved_valor">' + valor_aluno.value + '</div>'
+          + '</li>'
 
-        localStorage.setItem( 'cadastro_aluno', JSON.stringify(cadastro_aluno) );
+        localStorage.setItem( 'cadastro_aluno', alunos_cadastrados.innerHTML );
 
         nome_aluno.value = '';
         serie_aluno.value = '';
         valor_aluno.value = '';
       }
     });
+
+
+
+
+    $( '#cadastrar_bens' ).on( 'click', function ()
+    {
+      if ( nome_bens.value !== ''
+        && tipo_bens.value !== 'Selecione'
+        && valor_bens.value !== '' )
+      {
+        bens_cadastrados.innerHTML +=
+           '<li>'
+           + '<div class="saved_nome">' + nome_bens.value + '</div>'
+           + '<div class="saved_tipo">' + tipo_bens.value + '</div>'
+           + '<div class="saved_valor">' + valor_bens.value + '</div>'
+          + '</li>'
+
+        localStorage.setItem( 'cadastro_bens', bens_cadastrados.innerHTML );
+
+        nome_bens.value = '';
+        tipo_bens.value = 'Selecione';
+        valor_bens.value = '';
+      }
+    });
+
+
+
+    function show_cadastro_aluno ()
+    {
+      $('.fixed_right').hide();
+      $( '#alunos_cadastrados_holder' ).show();
+    }
+
+    function show_cadastro_bens ()
+    {
+      $('.fixed_right').hide();
+      $( '#bens_cadastrados_holder' ).show();
+    }
